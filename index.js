e7b9afca7e68ebd2bdc7586f74a872f9ff14301c
luisbossa <luisbossa@gmail.com>
2025-01-02 19:17:42 -0600

    npm update

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Import packages
-require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const cors = require("cors");
-
 
 const app = express();
 
@@ -12,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
